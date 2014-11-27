873c81c8cc0ab75911bdda9d4d1bfbb14948179c
Alvin Pascual <alvinpascual@superstar2.local>
2014-11-28 10:22:35 +1300

    Removed 5th state: Finished.

--- client/lib/states.js
+++ client/lib/states.js
@@ -3,31 +3,25 @@ item = [
   id: 1,
   name: "Delivery Options",
   templateName: "delivery",
-  width: "22%"
+  width: "25%"
 },
 {
   id: 2,
   name: "Your Order",
   templateName: "orders",
-  width: "17%"
+  width: "25%"
 },
 {
   id: 3,
   name: "Order Details",
   templateName: "detailsForm",
-  width: "20%"
+  width: "25%"
 },
 {
   id: 4,
   name: "Order Summary",
   templateName: "summary",
-  width: "22%"
-},
-{
-  id: 5,
-  name: "Finished",
-  templateName: "finished",
-  width: "19%"
+  width: "25%"
 }
 ];
 
@@ -36,7 +30,6 @@ _.sortBy(item,'id');
 
 // Constants for different states
 DELIVERY_STATE = 0;
-ORDERS_STATE = 1;
-DETAILS_STATE = 2;
-SUMMARY_STATE = 3;
-FINISHED_STATE = 4;
+ORDERS_STATE   = 1;
+DETAILS_STATE  = 2;
+SUMMARY_STATE  = 3;
