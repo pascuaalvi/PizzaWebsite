4be089c81f3e3e35bfc938f2c54b4a0c7f6e1c3b
Alvin Pascual <alvinpascual@superstar.local>
2014-11-28 13:10:07 +1300

    Fixed errors on recent merge. Duplicate code was found in files with different filenames.

--- client/templates/details/details.js
+++ client/templates/details/details.js
@@ -7,29 +7,18 @@ Template.details.helpers({
   },
   pickUp: function () {
     return Session.get('deliveryMode') === 'pickUp';
-  }
-});
-
-pickupOrDelivery = function (pickupOutput,deliveryOutput){
-  var mode = Session.get('deliveryMode');
-    if(mode === 'pickUp'){
-      return pickupOutput;
-    }
-    else if(mode === 'delivery'){
-      return deliveryOutput;
-    }
-}
+  },
   locationOptions: function() {
     return [
-        {label: "WellingtonCBD", value: "5"},
-        {label: "Outside WellingtonCBD", value: "10"}
+      {label: "WellingtonCBD", value: "5"},
+      {label: "Outside WellingtonCBD", value: "10"}
     ];
   }
 });
 
 Template.details.events({
-     'change #location': function(e,t){
-        var location = document.getElementById('location').value;
-        Session.set('location', location);
-     }
- });
+   'change #location': function(e,t){
+      var location = document.getElementById('location').value;
+      Session.set('location', location);
+   }
+});
