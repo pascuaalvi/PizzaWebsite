d48d172659aebc5263e0e6a68acf3957fafa5bb4
Alvin Pascual <alvinpascual@superstar.local>
2014-12-05 15:26:49 +1300

    Inserted a title and subtitle that automatically translates to French when the user profile's language is set to fr_FR. Can be extended to translate everything.

--- client/helpers/app.js
+++ client/helpers/app.js
@@ -1,5 +1,16 @@
 Meteor.startup(function () {
   Meteor.subscribe('allProducts');
+
+  if (Meteor.isClient)
+    if (Meteor.user()) {
+      language = Meteor.user().profile.language;
+    }
+    else {
+      //detect the language used by the browser
+      language = (window.navigator.userLanguage || window.navigator.language)
+      language = getLanguage(language);
+    }
+    i18n.setLanguage('fr_FR');
 });
 
 checkLogin = function (userId) {
